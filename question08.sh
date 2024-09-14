412f5e33362cf3bac51c905e7396ca9ff62e33be
S Lim <a1954354@student.adelaide.edu.au>
2024-09-14 23:08:35 +0930

    modified

--- question08.sh
+++ question08.sh
@@ -3,5 +3,8 @@
 #Move all text files (files ending in .txt) from the current directory to dir2, staging and committing your changes.
 mkdir dir2
 mv *.txt dir2
+git add dir2
+git commit -m "Moved all text files to dir2"
+
 
 
